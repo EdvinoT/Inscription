b813abf923f4de24458128f0153e521c57e4bf00
EdvinoT <edvinot82@gmail.com>
2025-12-02 14:20:16 +0000

    commit

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 people = ["Edvino","Anthony","Ed","Robin","Arhum"]
-print("Whom do you want to assign this project to?")
-print("Your choices are Edvino,Anthony,Ed,Robin,and Arhum")
+print("Who is signing in?")
+print("Is this Edvino,Anthony,Ed,Robin,or Arhum?")
 choice= input()
 if choice in people:
     print()
-    print(choice,",Has been chosen for this project.")
+    print("Welcome,",choice)
 else:
     print ("Person not avaiable")
+print()
 
+def content():
+    print("Hi,",choice)
+    print("Hope you enjoy learning python with us!")
+    print()
+    print("Best,")
+    print("The 3l337 AP Computer Science Team")
+    return content()
 
+file_inscrib= "inscribed.txt"
+with open(file_inscrib,"e" ) as file
